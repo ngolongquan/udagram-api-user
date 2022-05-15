86f78e7373111f29974f54d5d97e298d6ccab045
ngolongquan <60766273+ngolongquan@users.noreply.github.com>
2022-05-15 16:23:25 +0700

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+require('dotenv').config();
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0_USER_MODELS} from './controllers/v0/model.index';
+
+
+(async () => {  
+  await sequelize.addModels(V0_USER_MODELS);
+
+  console.debug("Initialize database connection...");
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+  // console.log ("port:"+port);
+  // console.log ("POSTGRES_USERNAME:"+process.env.POSTGRES_USERNAME);
+  // console.log ("POSTGRES_PASSWORD:"+process.env.POSTGRES_PASSWORD);
+  // console.log ("POSTGRES_DB:"+process.env.POSTGRES_DB);
+  // console.log ("POSTGRES_HOST:"+process.env.POSTGRES_HOST);
+  // console.log ("AWS_REGION:"+process.env.AWS_REGION);
+  // console.log ("AWS_PROFILE:"+process.env.AWS_PROFILE);
+  // console.log ("URL:"+process.env.URL);
+  // console.log ("JWT_SECRET:"+process.env.JWT_SECRET);
+
+  app.use(bodyParser.json());
+
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(cors({
+    allowedHeaders: [
+      'Origin', 'X-Requested-With',
+      'Content-Type', 'Accept',
+      'X-Access-Token', 'Authorization',
+    ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    preflightContinue: true,
+    origin: '*',
+  }));
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ${config.url}` );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
